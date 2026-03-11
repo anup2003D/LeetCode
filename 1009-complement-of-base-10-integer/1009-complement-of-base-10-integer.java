76a8194fd0d26eb5ac0de728bc48897bb71023a0
Anup Dutta <122201862+anup2003D@users.noreply.github.com>
2026-03-11 17:02:19 +0530

    Time: 12 ms (2.7%), Space: 42.1 MB (82.81%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] reverse(int[] arr){
+        int s=0;
+        int e=arr.length-1;
+        while(s<e){
+            int temp=arr[s];
+            arr[s]=arr[e];
+            arr[e]=temp;
+            s++; e--;
+        }
+        return arr;
+    }
+    public int bitwiseComplement(int n) {
+        if(n==0) return 1;
+        int x=n;
+        int count=0;
+
+        while(x>0){
+            x=x/2;
+            count++;
+        }
+        int[] arr=new int[count];
+        for(int i=0; i<count; i++){
+            arr[i]=n%2;
+            n/=2;
+        }
+        reverse(arr);
+        // System.out.println(x+" "+n);
+        for(int i=0; i<count; i++){
+            System.out.println(arr[i]);
+            arr[i]=Math.abs(arr[i]-1);
+            //System.out.println(arr[i]);
+        }
+        for(int i=count-1; i>=0; i--){
+            n+=arr[i]*Math.pow(2,x++);
+        }
+        return n;
+    }
+}

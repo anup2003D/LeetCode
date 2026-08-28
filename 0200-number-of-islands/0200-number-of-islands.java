a5f4c74941991b91beca96a1a0ce68dcd945d1f3
Anup Dutta <122201862+anup2003D@users.noreply.github.com>
2026-08-29 03:20:30 +0530

    Time: 2 ms (99.88%), Space: 52.4 MB (32.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,45 +1,36 @@
 class Solution {
     public void explore(char[][] grid, int i, int j){
-        int row = grid.length;
-        int column = grid[0].length;
 
-        if(i<0 || i>=row || j<0 || j>=column){
-            return;
-        }
-        
-        if (grid[i][j] == '0') {
-            return;
-        }
-        grid[i][j] = '0';
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length) return ;
+        if(grid[i][j]=='0') return;
 
-                   //up
-            explore(grid, i-1, j);
-        
-                    //down
-            explore(grid, i+1, j);
-        
-                   //left
-            explore(grid, i, j-1);
-        
-                   //right
-            explore(grid, i, j+1);
-        
+        grid[i][j]='0';
+
+        explore(grid, i-1, j);
+        explore(grid, i+1, j);
+        explore(grid, i, j-1);
+        explore(grid, i, j+1);
     }
     public int numIslands(char[][] grid) {
-        int row = grid.length;
-        int column = grid[0].length;
+        int rows = grid.length;
+        int cols = grid[0].length;
         int count=0;
-        
-        for(int i=0; i<row; i++){
-            for(int j=0; j<column; j++){
-                
+
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
                 if(grid[i][j]=='1'){
                     count++;
                     explore(grid, i, j);
                 }
-                
             }
         }
         return count;
     }
 }
+
+
+
+
+
+
+
